4ee84ca2b4cd0beebbc108ad428e74969c1b44f2
jone601214 <106021373@live.asia.edu.tw>
2017-09-30 16:10:05 +0800

    20170930

---
+++ src/大寫幻小寫.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class 大寫幻小寫 {
+    public static  void  main(String[]args){
+        Scanner scn=new Scanner(System.in);
+        int c=scn.nextInt();
+        System.out.println(Math.round(c*9/5f+32));
+        String str ="ASIA,FCU,NCHU";
+        char ch =',';
+        System.out.println(str.replace(',','\n'));
+    }
+}
